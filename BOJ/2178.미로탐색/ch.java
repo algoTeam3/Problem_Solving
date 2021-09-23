3495dd2c77cb2ef94fd82bf75ba24876d4ac62c0
박창현 <80505099+BusChanny@users.noreply.github.com>
2021-09-23 22:32:17 +0900

    BOJ 2178 미로탐색

---
+++ BOJ/2178.미로탐색/ch.java
@@ -0,0 +1,60 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_2178_미로탐색 {
+    static int N, M;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] map;
+    static boolean[][] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N  = Integer.parseInt(st.nextToken());
+        M  = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        bfs(0, 0);
+
+        System.out.println(map[N - 1][M - 1]);
+    }
+
+    private static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        visited[x][y] = true;
+        queue.offer(new int[] {x,y});
+
+        while (!queue.isEmpty()){
+            int[] arr = queue.poll();
+            x = arr[0];
+            y = arr[1];
+
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if (!visited[nx][ny] && map[nx][ny] == 1) {
+                    queue.offer(new int[] {nx,ny});
+                    visited[nx][ny] = true;
+                    map[nx][ny] = map[x][y] + 1;
+                }
+            }
+        }
+    }
+}

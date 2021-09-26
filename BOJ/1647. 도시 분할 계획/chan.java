670362b9d3a90e3658a9d6359e85382af632a1d0
Chae-EunJeong <68576770+Chae-EunJeong@users.noreply.github.com>
2021-09-27 01:14:11 +0900

    BOJ_1647_도시분할계획 문제 풀이 완료

---
+++ BOJ/1647. 도시 분할 계획/chan.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class chan {
+
+	static class Edge implements Comparable<Edge> {
+		int start, end, weight;
+		public Edge (int start, int end, int weight) {
+			this.start = start;
+			this.end = end;
+			this.weight = weight;
+		}
+		@Override
+		public int compareTo(Edge o) {
+			return this.weight - o.weight;
+		}
+	}
+	static int N, M;
+	static Edge[] edgeList;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		edgeList = new Edge[M];
+		
+		for (int m = 0; m < M; m++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			edgeList[m] = new Edge(start, end, weight);
+		}
+		
+		Arrays.sort(edgeList);
+		
+		make();
+		
+		int cnt = 0;
+		int result = 0;
+		for (Edge edge : edgeList) {
+			if (union(edge.start, edge.end)) {
+				result += edge.weight;
+				if (++cnt == N - 2) break;
+			}
+		}
+		System.out.println(result);
+	}
+	static int[] parents;
+	
+	private static void make() {
+		parents = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			parents[i] = i;
+		}
+	}
+	
+	private static int find(int a) {
+		if (parents[a] == a) return a;
+		return parents[a] = find(parents[a]);
+	}
+	
+	private static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		if (aRoot == bRoot) return false;
+		
+		parents[bRoot] = aRoot;
+		return true;
+	}
+}

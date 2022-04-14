e15ee13a978d1cba2c966b6c4ff21cba51731191
mindu23 <mindj5541@google.com>
2022-04-14 19:01:19 +0900

    solved programmers 탐욕법/체육복

--- programmers/코딩테스트연습/탐욕법(Greedy)/체육복/mj.java
+++ programmers/코딩테스트연습/탐욕법(Greedy)/체육복/mj.java
@@ -1,97 +1,72 @@
 import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
-    rows x columns 크기의 행렬
-    1부터 숫자가 row 한줄씩 순서대로 적혀있다
-    직사각형 모양의 범위를 여러번 선택해, 테두리 부분에 있는 숫자들을 시계방향으로 회전
-    각 회전은 x1,y1,x2,y2
+    학생의 번호는 체격 순. 바로 앞번호나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있다.
+    체육복을 적절히 빌려 최대한 많은 학생이 체육 수업을 들어야 한다.
+
  */
-public class D10_행렬테두리회전하기 {
+public class D10_체육복 {
 
     public static void main(String[] args) {
-        int rows = 6;
-        int columns = 6;
-        int[][] queries = {
-                {2,2,5,4},
-                {3,3,6,6},
-                {5,1,6,3}
-        };
-        int[] solution = solution(rows, columns, queries);
-        System.out.println(Arrays.toString(solution));
+        int n = 5;
+        int[] lost = {2,4};
+        int[] reserve = {1,3,5};
+
+        int solution = solution(n, lost, reserve);
+        System.out.println(solution);
+
     }
 
     /**
-     * queries 하나만 본다면 x1행 y1열부터 y2열까지
-     * y2열 x1행부터 x2행까지
-     * x2행 y2열부터 y1열까지
-     * y1행 x2행부터 x1행까지
-     * @param rows
-     * @param columns
-     * @param queries
-     * @return 위치가 바뀐 숫자들 중 가장 작은 숫자들을 순서대로 담은 배열 queries의 개수만큼
-     */
-    static int[][] map;
+     * 배열은 모두 오름차순
+     * @param n 전체 학생의 수
+     * @param lost 채육복을 도난당한 학생들의 번호 배열
+     * @param reserve 여벌의 체육복을 가져온 학생들의 번호 배열
+     * @return 수업을 들을 수 있는 학생의 최대값
+    */
+    private static int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
 
-    private static void init(int rows, int columns){
-        map = new int[rows+1][columns+1];
-        int k = 1;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= columns; j++) {
-                map[i][j] = k++;
+        // 원래 모두 하나씩
+        int[] check = new int[n+1];
+        Arrays.fill(check, 1);
+
+        // 도난당한 경우 0
+        for (int i = 0; i < lost.length; i++) {
+            check[lost[i]]--;
+        }
+
+        // 도난 당했지만 여벌 있는 경우 1
+        // 도난 당했고 여벌 없는 경우 0
+        for (int i = 0; i < reserve.length; i++) {
+            check[reserve[i]]++;
+        }
+
+        // 체육복이 없는 사람
+        LinkedList<Integer> lost2 = new LinkedList<>();
+        LinkedList<Integer> reserve2 = new LinkedList<>();
+        for (int i = 1; i < check.length; i++) {
+            if (check[i] == 0) lost2.add(i);
+            else if (check[i] == 2) reserve2.add(i);
+        }
+
+        for (int i = 0; i < reserve2.size(); i++) {
+            for (int j = 0; j < lost2.size(); j++) {
+                if (lost2.get(j) == -1) continue;
+                if (Math.abs(reserve2.get(i)-lost2.get(j)) == 1){
+                    reserve2.set(i, -1);
+                    lost2.set(j, -1);
+                    break;
+                }
             }
         }
-    }
 
-    private static int[] solution(int rows, int columns, int[][] queries) {
-        int[] answer = new int[queries.length];
-        init(rows,columns);
-
-        for (int i = 0; i < queries.length; i++) {
-            answer[i] = move(queries[i]);
+        int k = 0;
+        for (Integer integer : lost2) {
+            if (integer != -1) k++;
         }
 
-        return answer;
-    }
-
-    private static int move(int[] query) {
-        int x1 = query[0];
-        int y1 = query[1];
-        int x2 = query[2];
-        int y2 = query[3];
-
-        int temp1 = 0, temp2 = 0;
-        int min = Integer.MAX_VALUE;
-
-        // x1행 y1열부터 y2열까지
-        temp1 = map[x1][y2];
-        for (int i = y2; i > y1; i--) {
-            min = Math.min(min, map[x1][i]);
-            map[x1][i] = map[x1][i-1];
-        }
-
-        // y2열 x1행부터 x2행까지
-        temp2 = map[x2][y2];
-        for (int i = x2; i > x1; i--) {
-            min = Math.min(min, map[i][y2]);
-            map[i][y2] = map[i-1][y2];
-        }
-        map[x1+1][y2] = temp1;
-
-        // x2행 y2열부터 y1열까지
-        temp1 = map[x2][y1];
-        for (int i = y1; i < y2; i++) {
-            min = Math.min(min, map[x2][i]);
-            map[x2][i] = map[x2][i+1];
-        }
-        map[x2][y2-1] = temp2;
-
-        // y1행 x2행부터 x1행까지
-        for (int i = x1; i < x2; i++) {
-            min = Math.min(min, map[i][y1]);
-            map[i][y1] = map[i+1][y1];
-        }
-        map[x2-1][y1] = temp1;
-
-        return min;
+        return n-k;
     }
 }
